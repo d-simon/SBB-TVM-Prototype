02221bd31296c9592554afb79a1489587edf123c
David Simon <me@davidsimon.ch>
2014-06-04 13:15:05 +0200

    [Feature] Footer / [Refactor] Header into same format as footer

--- public/app/root/root.js
+++ public/app/root/root.js
@@ -12,9 +12,11 @@
                                 templateUrl: 'app/root/root.tpl.html'
                             },
                             'header@root': {
-                                templateUrl: 'app/root/root-header.tpl.html'
+                                templateUrl: 'app/root/header/header.tpl.html'
                             },
-                            'footer@root': {},
+                            'footer@root': {
+                                templateUrl: 'app/root/footer/footer.tpl.html'
+                            }
                         }
                     });
             }
