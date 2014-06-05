8465ed0d3f90186a464a14fca55d0a37a6ba8ba2
David Simon <me@davidsimon.ch>
2014-06-06 01:36:05 +0200

    [Fix] footer: correct binding in controller (to service not to state.obj)

--- public/app/root/footer/footer.js
+++ public/app/root/footer/footer.js
@@ -4,8 +4,7 @@
     angular.module('tvmProtoApp.root')
         .controller('FooterCtrl' ,['$scope', 'StateDataService',
             function ($scope, stateSrv) {
-                $scope.state = stateSrv.state;
-                console.log($scope.state);
+                $scope.stateSrv = stateSrv;
             }
         ]);
 
